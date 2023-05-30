42c327b25ef6060d4b9dfe451441b59b44d48f79
Jayplect <evansja.femi@gmail.com>
2023-05-30 07:25:30 -0400

    formatted legend

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -17,11 +17,12 @@ function createFeatures(earthquakeData) {
             layer.bindPopup(
                 `<h3>${feature.properties.place}</h3>
                 <hr><p>${new Date(feature.properties.time)}</p>`
+            // layer.on
     )};
 
     // Define a markerSize() function that will give each city a different radius based on its population.
     function markerSize(magnitude){
-        return Math.sqrt(magnitude)*3.5}
+        return Math.sqrt(magnitude)*4}
 
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
         // Run the onEachFeature function once for each piece of data in the array.
@@ -37,7 +38,7 @@ function createFeatures(earthquakeData) {
         var options = {
                 radius: markerSize(feature.properties.mag),
                 fillColor: chooseColor(feature.geometry.coordinates[2]),
-                color: "#000",
+                color: chooseColor(feature.geometry.coordinates[2]),
                 weight: 1,
                 opacity: 1,
                 fillOpacity: 0.8
@@ -47,10 +48,10 @@ function createFeatures(earthquakeData) {
 
     // Create a function to determine the depth of the earthquake by color
     function chooseColor(depth) {
-            if (depth < 50) {return "rgb(230, 255, 230)"}
-            else if (depth < 100) {return "rgb(153, 255, 153)"}
-            else if (depth < 300) {return "rgb(77, 255, 77)"}
-            else if (depth <  800) {return "rgb(0, 179, 0)"}
+            if (depth < 20) {return "rgb(230, 255, 230)"}
+            else if (depth < 50) {return "rgb(153, 255, 153)"}
+            else if (depth < 100) {return "rgb(77, 255, 77)"}
+            else if (depth <  200) {return "rgb(0, 179, 0)"}
             else  {return "rgb(0, 102, 0)"}
 };
 
@@ -79,43 +80,36 @@ function createMap(earthquakes) {
         zoom: 3,
         layers: [street, earthquakes]
     });
-    
-    // Setting up the legend.
-    let legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-        let div = L.DomUtil.create("div", "info legend");
-        let limits = geojson.options.limits;
-        let colors = geojson.options.colors;
-        let labels = [];
-
-        // Add the minimum and maximum.
-        let legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
-        "<div class=\"labels\">" +
-            "<div class=\"min\">" + limits[0] + "</div>" +
-            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
-
-        div.innerHTML = legendInfo;
-
-        limits.forEach((limit, index) => {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        });
-
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        return div;
-    };
-
-
     // Create a layer control. Pass it our baseMaps and overlayMaps.
     // Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap)
-};
+
+// Setting up the legend for earthquake magnitude.
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+    
+    var div = L.DomUtil.create('div', 'info legend'),
+        depth = [0, 20, 50, 100, 200],
+        labels = [];
+
+    // loop through our magnitude intervals and generate a label with a colored square for each interval
+    for (let i = 0; i < depth.length; i++) {
+            from = depth[i];
+            to = depth[i + 1];
+
+            labels.push(
+            '<i style="background:' + chooseColor(from ) + '"></i> ' +
+            from + (to ? '&ndash;' + to +'<br>' : '+'));
+            }
 
 
 
+        div.innerHTML += labels.join("") ;
+        return div;
+    };
   // Adding the legend to the map
   legend.addTo(myMap);
-
-
+}
