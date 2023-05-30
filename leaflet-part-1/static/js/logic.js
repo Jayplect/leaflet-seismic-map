87becb6c6f3b55242572284ca62559a1351fbb8f
Jayplect <evansja.femi@gmail.com>
2023-05-30 03:15:04 -0400

    added legend

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -1,4 +1,4 @@
-//-------------------------MONTHLY EARTHQUAKE MAP-------------------------//
+//-------------------------------------------MONTHLY EARTHQUAKE MAP---------------------------------------------//
 
 // Store our API endpoint as queryUrl.
 const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
@@ -9,7 +9,7 @@ d3.json(queryUrl).then(function (data) {
     createFeatures(data.features);
   });
 
-// 
+// Create features for Map
 function createFeatures(earthquakeData) {
 // Define a function that we want to run once for each feature in the features array.
 // Give each feature a popup that describes the place and time of the earthquake.
@@ -21,20 +21,7 @@ function createFeatures(earthquakeData) {
 
     // Define a markerSize() function that will give each city a different radius based on its population.
     function markerSize(magnitude){
-        return Math.sqrt(magnitude)*3}
-
-    // Creating a marker
-    function markerOptions (feature, latlng){
-        var options = {
-                    radius: markerSize(feature.properties.mag),
-                    fillColor: chooseColor(feature.geometry.coordinates[2]),
-                    color: "#000",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-        }
-        return L.circleMarker(latlng, options);
-};
+        return Math.sqrt(magnitude)*3.5}
 
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
         // Run the onEachFeature function once for each piece of data in the array.
@@ -44,7 +31,19 @@ function createFeatures(earthquakeData) {
         });
         // Send our earthquakes layer to the createMap function/
         createMap(earthquakes);
-};
+
+    // Creating a marker feature
+    function markerOptions (feature, latlng){
+        var options = {
+                radius: markerSize(feature.properties.mag),
+                fillColor: chooseColor(feature.geometry.coordinates[2]),
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+    }
+    return L.circleMarker(latlng, options);
+}};
 
     // Create a function to determine the depth of the earthquake by color
     function chooseColor(depth) {
@@ -55,7 +54,7 @@ function createFeatures(earthquakeData) {
             else  {return "rgb(0, 102, 0)"}
 };
 
-// 
+// Create the baselayer and overlay maps
 function createMap(earthquakes) {
 
     // Create the base layer.
@@ -77,15 +76,46 @@ function createMap(earthquakes) {
     // And Add to the div tag
     let myMap = L.map("map", {
         center: [37.09, -80],
-        zoom: 2,
+        zoom: 3,
         layers: [street, earthquakes]
     });
     
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
+    // Setting up the legend.
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = geojson.options.limits;
+        let colors = geojson.options.colors;
+        let labels = [];
+
+        // Add the minimum and maximum.
+        let legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
+        "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+
+        div.innerHTML = legendInfo;
+
+        limits.forEach((limit, index) => {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+
+    // Create a layer control. Pass it our baseMaps and overlayMaps.
     // Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
-    }).addTo(myMap);
-
+    }).addTo(myMap)
 };
+
+
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
+
+
